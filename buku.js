56ec84f3abd5a106d52ca53df2fd6fe03fa32c41
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2023-04-01 22:03:45 +0700

    Add files via upload

--- buku.js
+++ buku.js
@@ -4,12 +4,20 @@ class Buku {
       `https://api.hadith.gading.dev/books/bukhari?range=${dari}-${sampai}`
     );
     const { data } = await buku.json();
+    this.bacaan = ``
+    this.ket = ``
+    for(let i = 0; i < data.hadiths.length; i++) {
+      this.bacaan += `۝ ${data.hadiths[i].number}<br>${data.hadiths[i].arab} <hr>`
+      this.ket += `۝ ${data.hadiths[i].number}<br> ${data.hadiths[i].id} <hr>`
+      }
+      console.log(this.ket)
+
     this.judul = document.getElementById('judul')
     this.arab = document.getElementById("arab");
       this.teks = document.getElementById("teks");
-      this.judul.innerHTML = `${data.name}, nomor ${data.hadiths[0].number}` 
-      this.arab.innerText = data.hadiths[0].arab
-      this.teks.innerText = data.hadiths[0].id
+      this.judul.innerHTML = `${data.name}` 
+      this.arab.innerHTML = this.bacaan
+      this.teks.innerHTML = this.ket
     this.container = document.getElementById("container");
     this.container.addEventListener("click", function () {
       this.arab = document.getElementById("arab");
@@ -24,8 +32,12 @@ class Buku {
     
   }
 }
-let id = location.href;
-id = id.match(/\d+/)[0];
+const id = new URLSearchParams(window.location.search)
+const index = id.get('dari')
+const index2 = id.get('sampai')
+
+// let index = id.urlParams.get('id');;
+// let index2 = id.match(/\d+/)[1];
 
 let buku = new Buku();
-buku.getBuku(id, id);
+buku.getBuku(index, index2);
