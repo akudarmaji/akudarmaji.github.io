17efb5eef58f5a3ee52bde95f49ac0f21fc672aa
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2023-04-03 16:51:16 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ class Alquran {
             this.text = document.getElementById("text-tafsir");
             this.pTafsiran = document.getElementById("pTafsiran");
             this.pTafsiran.scrollTo({ top: 0, behavior: "smooth" });
-            this.text.innerText = ayahs[this.id].tafsir.quraish;
+            this.text.innerText = `Tafsir Quraish \n${ayahs[this.id].tafsir.quraish}\n\n Tafsir KEMENAG\n${ayahs[this.id].tafsir.kemenag.long}`;
             this.textTitle = document.getElementById("pAyat");
             this.textTitle.innerHTML = `Juz ${
               ayahs[this.id].meta.juz
@@ -98,7 +98,7 @@ function template(ayahs) {
   for (let i = 0; i < ayahs.length; i++) {
     arab += ` <button class="btn-audio border border-teal-300 relative z-10 shadow-xl h-6 w-15 -right-0 top-2 rounded-md bg-teal-100 bg-opacity-40 px-2  text-xs font-sm text-teal-500 font-bold" data-id="${i}">♬°</button>
     <button class="btn-tafsir border border-teal-300 relative z-10 shadow-xl h-6 w-15 -right-0 top-2 rounded-md bg-teal-100 bg-opacity-40 px-2 py-0 text-sm font-lateef font-bold font-sm text-teal-500" data-id="${i}">Ayat ${ayahs[i].number.inSurah}</button>
-    <div class="btn-terjemah bg-stone-100 hover:bg-white js-show-on-scroll ml-2 mr-2 bg-clip-padding backdrop-filter backdrop-blur-xl relative flex  justify-end rounded-xl border-double  border-4 border-teal-100 p-4 shadow-xl text-gray-700 sm:p-6 lg:p-8 transition transform hover:text-black hover:-translate-y-0.5 motion-reduce:transition-none motion-reduce:hover:transform-none text-right text-4xl font-lateef" data-id="${i}">
+    <div class="btn-terjemah bg-stone-100 hover:bg-stone-200 js-show-on-scroll ml-2 mr-2 bg-clip-padding backdrop-filter backdrop-blur-xl relative flex  justify-end rounded-xl border-double  border-4 border-teal-100 p-4 shadow-xl text-gray-700 sm:p-6 lg:p-8 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none text-right text-4xl font-lateef" data-id="${i}">
     <p class="">${ayahs[i].arab}</p>
    </div>`;
   }
