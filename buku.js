674e80304687e52f7c3fea8a670faf5ce43da0ae
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2023-04-03 17:29:41 +0700

    Add files via upload

--- buku.js
+++ buku.js
@@ -1,3 +1,10 @@
+let btnBackTop = document.getElementById('to-top-button')
+let backTop = document.getElementById('to-top-button')
+backTop.addEventListener('click', () => {
+  document.documentElement.scrollTo({ top: 0, behavior: "smooth" })
+})
+
+
 class Buku {
   async getBuku(dari, sampai) {
     let buku = await fetch(
@@ -7,8 +14,8 @@ class Buku {
     this.bacaan = ``
     this.ket = ``
     for(let i = 0; i < data.hadiths.length; i++) {
-      this.bacaan += `۝ ${data.hadiths[i].number}<br>${data.hadiths[i].arab} <hr>`
-      this.ket += `۝ ${data.hadiths[i].number}<br> ${data.hadiths[i].id} <hr>`
+      this.bacaan += `۩ ${data.hadiths[i].number}<br>${data.hadiths[i].arab} <hr>`
+      this.ket += `۞ ${data.hadiths[i].number}<br> ${data.hadiths[i].id} <hr>`
       }
       console.log(this.ket)
 
@@ -33,12 +40,24 @@ class Buku {
   }
 }
 
-window.addEventListener('load', function() {
+window.onload = () => {
   Telegram.WebApp.ready();
-  
   Telegram.WebApp.expand();
-})
+  document.documentElement.scrollTo({ top: 0, behavior: "smooth" })
+};
 
+function progress() {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+  let scrolled = Math.round((scrollTop / scrollHeight)*100)
+  btnBackTop.innerText = scrolled+'%'
+  if(scrolled == 100 || scrolled == 0) {
+    btnBackTop.classList.add('translate-y-12')
+  }else {
+    btnBackTop.classList.remove('translate-y-12')
+
+  }
+}
 
 const id = new URLSearchParams(window.location.search)
 const index = id.get('dari')
@@ -49,3 +68,4 @@ const index2 = id.get('sampai')
 
 let buku = new Buku();
 buku.getBuku(index, index2);
+window.onscroll = function(){progress()}
