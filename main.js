e5e6f5df0c6290be684de5ac612e9b13f8a7ac6a
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2023-04-07 15:39:55 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 const el = {
-  app: document.getElementById("app"),
-  audioAlafasy: document.getElementById("audio-alafasy"),
-  ayat: document.getElementById("p-ayat"),
   modal: document.getElementById("box-model"),
   textModal: document.getElementById("text-modal"),
   close: document.getElementById("closes"),
   topBtn: document.getElementById("to-top-button"),
-};
-
-const scrolling = {
   smooth: { top: 0, behavior: "smooth" },
-}
+};
 
 const Datas = {
   id: new URLSearchParams(window.location.search).get("id"),
@@ -18,73 +12,115 @@ const Datas = {
 };
 
 class Alquran {
-  async getQuran({ url, id }) {
-    let container = "";
-    const { number, numberOfAyahs, ayahs } = await (
-      await fetch(url + id)
-    ).json();
-    ayahs.forEach(
-      (
-        {
-          arab,
-          translation,
-          audio: { alafasy },
-          number: { inQuran, inSurah },
-          tafsir: { quraish, kemenag },
-        },
-        i
-      ) => {
-        container += template(arab, alafasy, translation, quraish);
-      }
-    );
-    el.app.innerHTML = container;
-    const btnAudio = document.querySelectorAll(".btn-audio");
-    btnAudio.forEach((btn) => {
-      btn.addEventListener("click", function () {
-        const audio = new Audio(this.dataset.alafasy)
-        audio.addEventListener('canplaythrough',function() {
-          audio.play()
-        })
-      });
-    });
-    const btnTafsir = document.querySelectorAll('.btn-tafsir')
-    btnTafsir.forEach(btn => {
-      btn.addEventListener('click', function() {
-        el.modal.scrollTo(scrolling.smooth);
-        el.modal.classList.remove('invisible')
-        el.textModal.innerText = this.dataset.tafsir
-      })
+  async fetching(url) {
+    const {ayahs} = await (await fetch(url)).json()
+    this.audio = []
+    this.arab = []
+    this.translation = []
+    this.kemenag = []
+    this.quraish = []
+    this.juz = []
+    this.page = []
+    this.inSurah = []
+    this.inQuran = []
+    ayahs.forEach(({arab,audio:{alafasy},tafsir:{kemenag:{long},quraish},meta:{juz,page},number:{inSurah,inQuran},translation}) => {
+      this.arab.push(arab)
+      this.audio.push(alafasy)
+      this.translation.push(translation)
+      this.kemenag.push(long)
+      this.quraish.push(quraish)
+      this.juz.push(juz)
+      this.page.push(page)
+      this.inSurah.push(inSurah)
+      this.inQuran.push(inQuran)
     })
-    const btnSub = document.querySelectorAll('.btn-sub')
-    btnSub.forEach(btn => {
+   
+  }
+
+}
+class Arabiyah extends Alquran{
+  constructor({url,id}) {
+    super()
+    this.url = url
+    this.id = id
+  }
+  async init() {
+   await   this.fetching(this.url+this.id)
+  }
+  getTemplate() {
+    let app = ``
+    this.arab.forEach((e, i) => {
+      app+= `<div class="app mb-1 bg-stone-100 hover:bg-stone-200 js-show-on-scroll ml-2 mr-2 bg-clip-padding backdrop-filter backdrop-blur-xl flex flex-row-reverse rounded-xl border-double  border-4 border-teal-100 p-4 shadow-xl text-gray-700 sm:p-6 lg:p-8 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none text-right text-2xl tracking-wider leading-relaxed font-lateef" data-id="${i}">
+      <div class"" id="arab${i}">${e}</div>
+      <button class="btn-tafsir border border-teal-300 absolute shadow-xl h-6  left-7 -top-3 rounded-md bg-teal-100 bg-opacity-40 px-2  text-sm text-teal-500 font-extralight" data-tafsir="${i}">Tafsir</button>
+      <button class="btn-sub border border-teal-300 absolute shadow-xl h-6  right-7 -top-3 rounded-md bg-teal-100 bg-opacity-40 px-2  text-sm text-teal-500 font-extralight" data-sub="${i}">Sub</button>
+             <button class="btn-audio border border-teal-300 absolute shadow-xl h-6  -right-2 -top-3 rounded-md bg-teal-100 bg-opacity-40  px-2 text-sm text-teal-500 font-extralight" data-alafasy="${i}">♬°</button>
+             
+        </div>`
+    })
+    document.getElementById('app').innerHTML = app
+  }
+  getAudio() {
+    const btnAudio = document.querySelectorAll('.btn-audio')
+    btnAudio.forEach(btn  => {
+      const audio = this.audio
       btn.addEventListener('click', function() {
-        el.modal.scrollTo(scrolling.smooth);
-        el.modal.classList.remove('invisible')
-        el.textModal.innerText = this.dataset.sub
+        new Audio(audio[this.dataset.alafasy]).play()
       })
     })
   }
+  getSub() {
+    const btnSub = document.querySelectorAll('.btn-sub')
+    btnSub.forEach((btn, i)  => {
+      
+      const sub = this.translation
+      const arab = this.arab
+      const juz = this.juz
+      const page = this.page
+      const inSurah = this.inSurah
+      const inQuran = this.inQuran
+      const divarab = document.getElementById(`arab${i}`)
+      btn.addEventListener('click', function(e) {
+        const id = this.dataset.sub
+        if(divarab.innerText === arab[id]) {
+          divarab.innerText = `${sub[id]}\nJuz ${juz[id]}, Hal ${page[id]}, Nomor Surat ${inSurah[id]}, Nomor Ayat ${inQuran[id]}`
+          divarab.classList.add('text-sm','text-justify','leading-5')
+        }else {
+          divarab.innerText = arab[id]
+          divarab.classList.remove('text-sm','text-justify','leading-5')
+        }
+ 
+      })
+    })
+  }
+  getTafsir() {
+    const btnTaf = document.querySelectorAll('.btn-tafsir')
+    btnTaf.forEach(btn  => {
+      const kemenag = this.kemenag
+      const quraish = this.quraish
+      btn.addEventListener('click', function() {
+        const id = this.dataset.tafsir
+        el.modal.classList.remove('invisible')
+        el.textModal.innerText = `QURAISH \n${quraish[id]}\nKEMENAG\n${kemenag[id]}`
+      })
+    })
+  }
+  closes() {
+    el.modal.classList.add('invisible')
+  }
+  backToTop() {
+    document.documentElement.scrollTo(el.smooth)
+  }
 }
 
-function template(arab, alafasy, translation, quraish) {
-  return `<div class="mb-1 bg-stone-100 hover:bg-stone-200 js-show-on-scroll ml-2 mr-2 bg-clip-padding backdrop-filter backdrop-blur-xl flex flex-row-reverse rounded-xl border-double  border-4 border-teal-100 p-4 shadow-xl text-gray-700 sm:p-6 lg:p-8 transition transform motion-reduce:transition-none motion-reduce:hover:transform-none text-right text-2xl tracking-wider leading-relaxed font-lateef">${arab}
+const arabiyah = new Arabiyah(Datas)
+arabiyah.init().then(() => {
+  arabiyah.getTemplate()
+  arabiyah.getAudio()
+  arabiyah.getSub()
+  arabiyah.getTafsir()
+})
 
-             <button class="btn-tafsir border border-teal-300 absolute shadow-xl h-6 left-0 -top-3 rounded-md bg-teal-100 bg-opacity-40 px-2  text-sm text-teal-500 font-extralight" data-tafsir="${quraish}">Tafsir</button>
-             <button class="btn-sub border border-teal-300 absolute shadow-xl h-6  right-7 -top-3 rounded-md bg-teal-100 bg-opacity-40 px-2  text-sm text-teal-500 font-extralight" data-sub="${translation}">Sub</button>
-             <button class="btn-audio border border-teal-300 absolute shadow-xl h-6  -right-2 -top-3 rounded-md bg-teal-100 bg-opacity-40  px-2 text-sm text-teal-500 font-extralight" data-alafasy="${alafasy}">♬°</button>
-               </div>`;
-}
-
-
-function closes() {
-  el.modal.classList.add('invisible')
-}
-
-window.onload = () => {
-  Telegram.WebApp.ready();
-  Telegram.WebApp.expand();
-  document.documentElement.scrollTo(scrolling.smooth);
-};
 
 window.onscroll = function() {
   let scrollTop = document.documentElement.scrollTop;
@@ -99,6 +135,3 @@ window.onscroll = function() {
     el.topBtn.classList.remove("translate-y-12");
   }
 }
-
-const alquran = new Alquran();
-alquran.getQuran(Datas, el);
