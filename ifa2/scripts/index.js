6fad3eb35e968eb73d96da7dbf50878cb323be3b
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2024-07-29 19:13:45 +0700

    Update index.js

--- ifa2/scripts/index.js
+++ ifa2/scripts/index.js
@@ -1,7 +1,21 @@
+const panels = document.querySelectorAll(".panel");
 const count = document.getElementById('count');
 const head = document.getElementById('head');
 const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
 
 const config = {
   birthdate: 'Aug 8, 2023',
