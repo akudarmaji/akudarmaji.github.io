a921ce5eaec8c1411f438a3ae4d81a6058abeda7
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2024-07-29 11:50:49 +0800

    Update hitungmundur.js

--- ifa/countdown/hitungmundur.js
+++ ifa/countdown/hitungmundur.js
@@ -1,1 +1,26 @@
-
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+​
+var bc = document.createElement("canvas");
+var bCtx = bc.getContext("2d");
+​
+var cw = c.width = bc.width = window.innerWidth,
+  cx = cw / 2;
+var ch = c.height = bc.height = window.innerHeight + 100,
+  cy = ch;
+​
+var frames = 0;
+var requestId = null;
+var rad = (Math.PI / 180);
+var kappa = 0.5522847498;
+​
+var x, y;
+bCtx.strokeStyle = "#abcdef";
+bCtx.lineWidth = 1;
+​
+var balloons = [];
+​
+function Balloon() {
+  this.r = randomIntFromInterval(20, 70);
+  this.R = 1.4 * this.r;
+  this.x = randomIntFromInterval(this.r, cw - this.r);
