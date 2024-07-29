9afcf0b66c18777a5fb4306328f1c4d889fb80f7
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2024-07-29 14:25:54 +0800

    Update index.js

--- ifa2/scripts/index.js
+++ ifa2/scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 8, 2024',
-  name: 'Darlene'
+  birthdate: 'Aug 8, 2023',
+  name: 'Sayang'
 };
 
 function hideEverything() {
