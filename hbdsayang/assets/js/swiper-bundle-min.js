3e6c92443074d17f70234b95363d9111be1196de
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2024-07-30 20:23:04 +0800

    Update swiper-bundle-min.js

--- hbdsayang/assets/js/swiper-bundle-min.js
+++ hbdsayang/assets/js/swiper-bundle-min.js
@@ -9,6 +9,43 @@
  *
  * Released on: January 30, 2023
  */
+',100%,' + i + '%)'
+          ctx.arc(point.x, point.y, 1, 0, PI2, false)
+          ctx.fill()
+        } 
+      } else {
+      ctx.beginPath()
+      ctx.fillStyle = 'hsl(' + this.shade + ',100%,50%)'
+      ctx.arc(this.x, this.y, 1, 0, PI2, false)
+      ctx.fill()
+    }
+
+  }
+}
+
+let canvas = document.getElementById('birthday')
+let ctx = canvas.getContext('2d')
+
+let then = timestamp()
+
+let birthday = new Birthday
+window.onresize = () => birthday.resize()
+document.onclick = evt => birthday.onClick(evt)
+document.ontouchstart = evt => birthday.onClick(evt)
+
+  ;(function loop(){
+  	requestAnimationFrame(loop)
+
+  	let now = timestamp()
+  	let delta = now - then
+
+    then = now
+    birthday.update(delta / 1000)
+
+  })()
+
+*/endanimation*/
+  
 
 !(function (e, t) {
   "object" == typeof exports && "undefined" != typeof module
