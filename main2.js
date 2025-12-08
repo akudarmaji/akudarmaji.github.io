8e8b5c296b9cf15e4d562185f9e3b056d14676a5
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2025-12-08 21:31:30 +0800

    Implement fetch function for Quran verses
    Fetch data from Quran API and handle errors.

--- main2.js
+++ main2.js
@@ -1,1 +1,10 @@
-xsd
+const url = 'https://api.quran.com/api/v4/quran/verses/indopak?chapter_number=1';
+const fetch = async () => {
+  try { 
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data); 
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
