a0c168fafa28a007e469b9f336724602a6a7842c
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2026-01-24 23:14:19 +0800

    Add files via upload

--- alquran.js
+++ alquran.js
@@ -1,177 +1,96 @@
+import {fetchingData} from '/main-quran/fetchingData.js';
+import {show,seekBar} from '/main-quran/show.js';
+import {audioPause, loadAudio, playAud} from '/main-quran/loadAudio.js';
 
-  const id = new URLSearchParams(window.location.search).get("id")
-  let tafsirnya = [];
-const ula = document.getElementById('ayat');
-const audio =document.getElementById('audio');
-  let musicIndex = [];
 
-  window.onload = ()=> {
-    checkbox.checked =true
-     latinCheckbox.checked = true;
-     terjemahCheckbox.checked = true;
-     toolsCheckbox.checked = true;
-  }
-  const API_TAFSIR = 'https://equran.id/api/v2/tafsir/'
-  const API_URL = 'https://equran.id/api/v2/surat/'
-
-  async function fetchPosts(id) {
-    const response = await fetch(`${API_URL}${id}`)
-    let data = await response.json()
-    const response2 = await fetch(`${API_TAFSIR}${id}`)
-    let tafsir = await response2.json()
-    const tafsir2 = tafsir.data.tafsir;
-
-    if (response.ok) {
-      show(data,tafsir2);
-      document.getElementById('loading').style.display = 'none'
-        
-    }
-  }
-    
-  function show({data},tafsir) {
-   // const previus = data.suratSebelumnya.nomor
-    const {ayat, nama, nomor, namaLatin,jumlahAyat, arti} = data;
-    const judul = document.getElementById('nama')
-    judul.innerText = nama
-    const descHeader = document.getElementById('arti');
-descHeader.innerText = `${nomor} | ${namaLatin} | ${arti} | ${jumlahAyat} ayat`
+//const API_TAFSIR = 'https://equran.id/api/v2/tafsir/'
+  //const id = new URLSearchParams(window.location.search).get("id");
+ let id = 1;
   
-  for (let i = 0; i < ayat.length; i++) {
-    const url = ayat[i].audio
-    const audioURL = Object.values(url)[0]
-    
-      musicIndex.push(audioURL);
-      tafsirnya.push(tafsir[i].teks)
-  const noAyat = ayat[i].nomorAyat;
-  const number = new Intl.NumberFormat('ar-EG').format(noAyat)
-    
-  const liTagAll = `<li li-index=${i} class="li-container">
-    <div class="tools-container">
-  <div onclick="desc(this)" id="${noAyat}" class="tools1">${noAyat}</div>
-  <div id="tool" class="tools2"></div>
-  <div id="tool" class="tools3"></div>
-</div>
-      <p class='latin'>${ayat[i].teksLatin}</p>
-      <p class='terjemah'>${ayat[i].teksIndonesia}</p>
-      <h1 class='arab'>${ayat[i].teksArab}
-      </h1>
-      </li>`;
-    ula.insertAdjacentHTML('beforeend', liTagAll);
-    } 
-  }
-
-
-let seekbar = document.createElement('input');
-seekbar.setAttribute('class', 'seek-bar')
-seekbar.setAttribute('type', 'range')
-
-ula.addEventListener('click', (e) => {
-  const seekBars = ula.querySelectorAll('h1 input')
-  const parent = e.target.parentElement
-  const index = parent.getAttribute('li-index');
-    audio.src = musicIndex[index]
-
-  const lists = document.querySelectorAll('h1.arab');
-    lists.forEach((list)=> {
-      list.classList.remove('hover')
+  let indexGlobal = 0;
+  let audioUrl = [];
+  let lengthAyat = 0;
+  
+  const app = document.getElementById('app');
+  
+  window.addEventListener('load', loadPage(id),false)
+  
+  async function loadPage(id) {
+    const ayat = await fetchingData(id);
+    app.innerHTML = await show(ayat)
+    audioUrl =await loadAudio(ayat);
+    lengthAyat = await (ayat.length-1);
+    let arabs = document.querySelectorAll('.arab');
+    arabs.forEach((arab,i)=>{
+      arab.addEventListener('click', (e)=>{
+        indexGlobal = i;
+        const element = arabs[indexGlobal]
+      control(element, indexGlobal);
+      })
+  })
+  const humbers = document.querySelectorAll('#humber');
+  humbers.forEach((humber)=>{
+    humber.addEventListener('click',(e)=>{
     })
-  setTimeout(()=> {
-    if(audio.duration = NaN) {
-    seekbar.style.display ='none'
-    }else {
-      seekbar.max = Math.round(audio.duration)
-    }
-  },500)
-  setInterval(()=>{
-    if(seekbar.value = undefined) {
-      seekbar.style.display ='none'
-    }else {
-      seekbar.value = audio.currentTime;
-    }
-},100)
-
-  seekbar.addEventListener('change',()=>{
-    audio.currentTime = seekbar.value
   })
+  }
+  
+async  function control(element,indexGlobal) {
+    const audio = document.getElementById('audio');
+        audio.src = audioUrl[indexGlobal];
+        const playing = element.classList.contains('playing');
+        playing ? audioPause(element, indexGlobal) : playAud(element, indexGlobal);
+  }
  
-  const arab = e.target.className == 'arab'
-    arab ? active(e):stop(e);
-    
-  })
   
-  function active(e) {
-    e.target.appendChild(seekbar)
-    e.target.classList.add('hover')
-    audio.play()
-  }
-  function stop(e) {
-  e.target.classList.remove('hover')
-  }
-  
-  
-  window.onscroll = function() {scrollFunction()
-  };
-
-  function scrollFunction() {
-  percentageVal = document.querySelector("#percentage-value")
-  bodyHeight = document.body.offsetHeight
-  const heightOfWindow = window.innerHeight
-contentScrolled = window.pageYOffset;
-
-  const total = bodyHeight - heightOfWindow,
-  got = contentScrolled,
-  percent = parseInt((got / total) * 100)
-  const ball = document.getElementById("ball");
-ball.textContent = `${percent}`;
-}
-
-
-
-  const checkbox = document.getElementById("checkbox")
-  document.body.classList.add('dark')
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
+const audio = document.getElementById('audio');
+audio.addEventListener('timeupdate',()=>{
+  seekBar()
 })
-  
-const latinCheckbox = document.getElementById('latin-checkbox');
-  const listLatin = document.getElementsByClassName('latin');
-  latinCheckbox.addEventListener('change',()=>{
-    for (let a of listLatin) {
-      a.classList.toggle('hide');
-    }
-  })
- 
-  
-  
-  const terjemahCheckbox = document.getElementById('checkbox-terjemah');
-  const listTerjemah = document.getElementsByClassName('terjemah');
-  terjemahCheckbox.addEventListener('change',()=>{
-    for (let a of listTerjemah) {
-      a.classList.toggle('hide');
-    }
-  })
-  
- const toolsCheckbox = document.getElementById('tools');
-const listTools = document.getElementsByClassName('tools-container');
-toolsCheckbox.addEventListener('change', () => {
-  for (let a of listTools) {
-    a.classList.toggle('hide');
+audio.addEventListener('ended', async ()=>{
+  if (indexGlobal == lengthAyat) {
+  id++;
+  await loadPage(id);
+indexGlobal = 0;
+const arabs = document.querySelectorAll('.arab');
+      const element = await arabs[indexGlobal];
+  control(element, indexGlobal);
+  } else {
+    const arabs = document.querySelectorAll('.arab');
+          indexGlobal++;
+      const element =  arabs[indexGlobal]
+control(element, indexGlobal);
   }
 })
 
- const desc = function (e) {
-  e.classList.toggle('desc')
-  const id = e.getAttribute('id')
-  const teksContoh  = tafsirnya[id-1]
-  let teks = e.innerText
-  e.innerText = teks == id ? teksContoh : id;
-  
-  
- }
+const btnNext = document.getElementById('btn-next');
+btnNext.addEventListener('click', () => {
+  window.scrollTo({top:0})
+  id++;
+  loadPage(id);
+      document.getElementById('footer').style.visibility = 'hidden';
+  const audio = document.getElementById('audio');
+  audio.pause();
+})
 
-function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+const tools = document.getElementById('tools');
+tools.addEventListener('click',(e)=>{
+  console.log(e.target)
+})
+
+
+
+
+const toggle = document.querySelector("#toggle");
+toggle.addEventListener("click", modeSwitch);
+
+let isLight = true;
+
+function modeSwitch() {
+  isLight = !isLight;
+  let root = document.body;
+  
+ isLight ? toggle.innerText = "dark" : toggle.innerText = "light";
+  
+  root.classList.toggle("lightMode");
 }
-
-    fetchPosts(id)
