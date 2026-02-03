fc031097ecb770839cd42cba9e9ef804a47cd136
akudarmaji <104605592+akudarmaji@users.noreply.github.com>
2026-02-03 23:25:44 +0800

    Add files via upload

--- alquran.js
+++ alquran.js
@@ -33,24 +33,12 @@ const tafsir = document.getElementById("tafsir");
 const toggle = document.querySelector("#toggle");
 
 window.addEventListener("load", () => {
-    id = localStorage.idSurat;
     loadPage(id);
 });
 window.onscroll = () => {
     const btn = document.querySelector(".dropdown");
     btn.classList.remove("active");
 };
-//dropdown list surat////////
-window.addEventListener("DOMContentLoaded", async () => {
-    const titleArray = await fetchingTitle();
-    dropDown(titleArray);
-    titleOnChange.value = localStorage.idSurat ? localStorage.idSurat : id;
-    indexGlobal = Number(localStorage.idAyat);
-    selectAyat.value = Number(localStorage.idAyat) + 1;
-    const arabs = document.querySelectorAll(".arab");
-    const element = arabs[indexGlobal];
-    control(element, indexGlobal);
-});
 //render halaman awal --+--
 export async function loadPage(id) {
     const {data, ayat} = await fetchingData(id);
@@ -63,21 +51,15 @@ export async function loadPage(id) {
     arrTafsir = await fetchingTafsir(id);
     tafsir.checked = false;
     let arabs = document.querySelectorAll(".arab");
+    const humbers = document.querySelectorAll("#humber");
+    humbers.forEach(humber => {
+        humber.remove();
+    });
     arabs.forEach((arab, i) => {
         arab.addEventListener("click", e => {
             indexGlobal = i;
-localStorage.setItem("idSurat", id);
-        localStorage.setItem("idAyat", indexGlobal);
             const element = arabs[indexGlobal];
             control(element, indexGlobal);
-        });
-    });
-    const humbers = document.querySelectorAll("#humber");
-    humbers.forEach(humber => {
-        humber.addEventListener("click", e => {
-            const idAyat = e.target.parentElement.id;
-            localStorage.setItem("idAyat", idAyat);
-            localStorage.setItem("idSurat", id);
         });
     });
 }
@@ -98,8 +80,6 @@ audio.addEventListener("ended", async () => {
 
         await loadPage(id);
         indexGlobal = 0;
-        localStorage.setItem("idSurat", id);
-        localStorage.setItem("idAyat", indexGlobal);
         const arabs = document.querySelectorAll(".arab");
         const element = await arabs[indexGlobal];
         control(element, indexGlobal);
@@ -107,7 +87,6 @@ audio.addEventListener("ended", async () => {
     } else {
         const arabs = document.querySelectorAll(".arab");
         indexGlobal++;
-        localStorage.setItem("idAyat", indexGlobal);
         const element = arabs[indexGlobal];
         control(element, indexGlobal);
     }
@@ -127,25 +106,11 @@ function modeSwitch() {
 }
 
 //tag Select list Surat (114) selected surat
-titleOnChange.onchange = function () {
-    const loading = document.querySelector(".loader");
-    loading.style.display = "block";
-    id = this.value;
-    titleOnChange.value = id;
-    localStorage.setItem("idSurat", id);
-    loadPage(id);
-    const element = document.querySelectorAll(".arab")[indexGlobal];
-    audioPause(element);
-    window.scrollTo({
-        top: 80
-    });
-};
 
 //tag select list ayat; selected onPlay
 selectAyat.onchange = function () {
     indexGlobal = Number(this.value) - 1;
     selectAyat.value = indexGlobal;
-    localStorage.setItem("idAyat", indexGlobal);
     const arabs = document.querySelectorAll(".arab");
     const element = arabs[indexGlobal];
     control(element, indexGlobal);
